c6bd34fb3a11c8e624e8ed1b7ee4635dd5bde673
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-09-30 10:23:19 +0800

    Add CORS headers to response

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ function responseSuccess(res, data) {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 router.get('/count', (req, res) => {
     responseSuccess(res, {count: count});
